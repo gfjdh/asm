736e01e59111ae974d224b42181c3d9ddcb5c197
gfjdh1 <2556123498@qq.com>
2025-10-20 11:04:41 +0800

    完成loop方式

--- loop.asm
+++ loop.asm
@@ -1,23 +1,56 @@
-STKSEG SEGMENT STACK		; 定义堆栈段，用于程序运行时的堆栈空间
-	DW 32 DUP(0)			; 分配32个字(64字节)的堆栈空间，初始化为0
-STKSEG ENDS					; 堆栈段结束
+STKSEG SEGMENT STACK	; 定义堆栈段
+	DW 32 DUP(0)
+STKSEG ENDS
 
-; 定义数据段，用于存储程序的数据
 DATASEG SEGMENT
-	MSG DB "Hello World$"		; 定义字符串"Hello World$"，$符号是DOS字符串结束标记
-DATASEG ENDS					; 数据段结束
+	CRLF DB 13,10,'$'
+	CUR DB 'a'
+DATASEG ENDS
 
-; 定义代码段，包含程序的执行代码
 CODESEG SEGMENT
-	ASSUME CS:CODESEG,DS:DATASEG	; 告诉汇编器CS指向代码段，DS指向数据段
-MAIN PROC FAR				; 定义主程序，FAR表示远过程调用
-	MOV AX,DATASEG			; 将数据段的段地址加载到AX寄存器
-	MOV DS,AX			; 将AX的值赋给DS寄存器，设置数据段寄存器
-	MOV AH,9			; 设置DOS功能号9(显示字符串)到AH寄存器
-	MOV DX,OFFSET MSG		; 将字符串MSG的偏移地址加载到DX寄存器
-	INT 21H				; 调用DOS中断21H，执行字符串显示功能
-	MOV AX,4C00H			; 设置程序正常结束功能号(4CH)和返回码(00H)
-	INT 21H				; 调用DOS中断21H，结束程序并返回到DOS
-MAIN ENDP				; 主程序结束
-CODESEG ENDS				; 代码段结束
-	END MAIN			; 程序结束，指定程序入口点为MAIN
+	ASSUME CS:CODESEG,DS:DATASEG
+MAIN PROC FAR
+	MOV AX,DATASEG
+	MOV DS,AX
+
+	; 外层循环：行数（2 行，每行 13 字母）
+	MOV CX,2
+outer_loop:
+	; 保存外层 CX，因为内层也要使用 CX/LOOP
+	PUSH CX
+
+	; 内层循环：每行 13 个输出项
+	MOV CX,13
+inner_loop:
+	; 输出当前字母
+	MOV DL,[CUR]
+	MOV AH,02h
+	INT 21h
+
+	; 如果这是本行的最后一个（CX==1），打印换行；否则打印空格
+	CMP CX,1
+	JE .print_crlf
+	MOV DL,' '
+	MOV AH,02h
+	INT 21h
+	JMP .after_sep
+.print_crlf:
+	MOV AH,09h
+	MOV DX,OFFSET CRLF
+	INT 21h
+.after_sep:
+	; 递增字符
+	INC BYTE PTR [CUR]
+	LOOP inner_loop
+
+	; 恢复外层 CX 并由 LOOP 控制外层迭代
+	POP CX
+	LOOP outer_loop
+
+	; 程序正常结束
+	MOV AX,4C00H
+	INT 21H
+MAIN ENDP
+CODESEG ENDS
+	END MAIN
+
